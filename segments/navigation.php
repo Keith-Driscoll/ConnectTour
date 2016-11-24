fe4a738f5fb77a009007d68698c0fd9176247591
Keith <keithdriscoll16@gmail.com>
2016-11-24 16:28:55 +0000

    keith

--- segments/navigation.php
+++ segments/navigation.php
@@ -58,7 +58,7 @@ require_once 'getProfilePicture.php';
 	<div class="container clearfix">
 		<!-- main column -->					
 		<a href="index.php">
-			<h1 id="logo">ConnectTour</h1>
+			<h1 class="cd-logo" id="logo">ConnectTour</h1>
 		</a>
 		<nav>	
 			<a href="tournaments.php" class="">Compete</a>
