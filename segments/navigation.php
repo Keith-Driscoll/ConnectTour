84482b63a0a217c73f0ddeb347b9dbb09c736bfe
keith <keith@DESKTOP-2VIEUS0>
2016-11-29 08:47:07 +0000

    - keith

--- segments/navigation.php
+++ segments/navigation.php
@@ -65,7 +65,7 @@ require_once 'getProfilePicture.php';
 			<a href="support.php" class=""> Support</a>
 			<?php 
 			    //checks if user is logged in, if not, redirect to login page
-			  require_once 'classes/doLoginCheck.php';
+			  //require_once 'classes/doLoginCheck.php';
 			  require_once 'classes/connections.php';
 			  $db_connection = db_connect();
 			?>
