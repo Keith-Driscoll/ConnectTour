257afc00fd0f92b05d7a3f277ab85a74fb3ab864
keith <keith@DESKTOP-2VIEUS0>
2016-11-29 09:15:30 +0000

    connection - Keith

--- classes/connections.php
+++ classes/connections.php
@@ -3,7 +3,7 @@
 		//server1 servername -> gglmysql.cloudapp.net (active)
 		//server2 servername -> gglmysql2.cloudapp.net
 		$servername = "127.0.0.1";
-		$username = "azure@localhost";
+		$username = "ct_admin";
 		$password = "Password123";
 		$dbname = "ggl_main";
 
@@ -12,7 +12,7 @@
 		// Check connection
 		if (mysqli_connect_error()) {
 		    die("Connection failed: " . mysqli_connect_error());
-		    exit();
+		    //exit();
 		} else {
 			echo "Connection successful <br>";
 			return $conn;
