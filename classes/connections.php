691200644a0015578bb2215d198c0c3c1dc9cbae
keith <keith@DESKTOP-2VIEUS0>
2016-11-29 08:52:25 +0000

    output error/success message on connection attempt - Keith

--- classes/connections.php
+++ classes/connections.php
@@ -11,10 +11,10 @@
 		$conn = new mysqli($servername, $username, $password, $dbname);
 		// Check connection
 		if (mysqli_connect_error()) {
-		    //die("Connection failed: " . mysqli_connect_error());
+		    die("Connection failed: " . mysqli_connect_error());
 		    exit();
 		} else {
-			//echo "Connection successful <br>";
+			echo "Connection successful <br>";
 			return $conn;
 		}
 	}
