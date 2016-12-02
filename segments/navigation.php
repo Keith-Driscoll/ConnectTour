9fba96233b6aa5c43e35ecbaa3f119032d3d25db
keith <keith@DESKTOP-2VIEUS0>
2016-12-02 15:53:47 +0000

    navbar - keith

--- segments/navigation.php
+++ segments/navigation.php
@@ -14,23 +14,8 @@ Last modified  7/3/2016 - Keith
 <script src='../js/jquery.min.js'></script>
 <script src="js/jquery.menu-aim.js"></script>
 <script src="js/main.js"></script>
-<!--
-<script>
-	function getNotifications(){
-	var p_id = <?=$p_id?>;
-	$.ajax({
-		type: "GET",
-		url: "get_notifications.php",
-		data: {p_id:p_id},
-		success: function(data){
-			if(data!=''){
-				var text = document.getElementById("notifications").innerHTML;
-				var res = text.split('(');
-				var newstring = ''+res[0] + '(' + data + ')';
-				document.getElementById("notifications").innerHTML = newstring;
-			}
-		}
-	});		
+
+ <script>
 
 	function sessionTimer(){
 		var p_id = <?=$p_id?>;
@@ -41,64 +26,69 @@ Last modified  7/3/2016 - Keith
 	}
 	
 	$(function() {
-			var notificationTimer = setInterval(getNotifications, 600000);	
 			//var sessionTime = setInterval(sessionTimer,18000000); //30 minute timeout
 	});
 	
 </script>
--->
-<header class="cd-main-header">
-		<a href="../index.php">
-			<h1 id="cd-logo">GGLeagues</h1>
+<header>
+	<!-- row -->
+	<div class="container clearfix">
+		<!-- main column -->					
+		<a href="index.php">
+			<h1 id="logo">GGLeagues</h1>
 		</a>
-		<!-- 
-		<div class="cd-search is-hidden">
-			<form action="#0">
-				<input type="search" placeholder="Search...">
-			</form>
-		</div>
-		-->
-
-		<a href="#0" class="cd-nav-trigger">Menu<span></span></a>
-
-		<nav class="cd-nav">
-			<ul class="cd-top-nav">
-				<li><a href="../tournaments.php">Compete</a></li>
-				<li><a href="../support.php">Support</a></li>
-				<li class="has-children account">
-					<a href="#0">
-						<?php include 'getProfilePicture.php'; $pic = getProfilePicture($_SESSION['user_id']); ?>
-							<?php echo"
-							<img src='".$pic."' alt='avatar'/>
-							"
-							?>
-							<?php echo "".$_SESSION['user_name']?>
-					</a>
-
-					<ul>
-
-						<li>
-							<?php echo "<a href='profile.php?id=".$_SESSION['user_id']."'>My Account</a>";?>
-						</li>
-						<li>
-							<?php //echo "<a href='../profile.php?id=".$_SESSION['user_id']."&page=notifications'>"; ?>Notifications 
-								<?php
-								//print number of notifications
-                                /*
-								$db_connection = db_connect();
-								$sql = "SELECT * FROM notifications WHERE p_id = ".$_SESSION['user_id']." AND is_read=0";
-								$result = $db_connection->query($sql);
-								echo " (".$result->num_rows.")";
-								*/?>
-							</a>
-						</li>
-						<li>
-							<form method="post">
-								<a href="index.php?logout" class="">Sign out</a>
-							</form>
-						</li>
-					</ul>
-				</li>
-			</ul>
+		<nav>	
+			<a href="tournaments.php" class="">Compete</a>
+			<a href="support.php" class=""> Support</a>
+			<a href="../login.php" class="login-btn">Login</a>
+			
+			
+						
+			
 		</nav>
-	</header><!-- ./ header end-->
+	</div><!-- ./row end -->	
+</header><!-- ./ header end-->
+	
+<aside id="side-nav" class="hide">
+		<ul>
+			<li class="x">
+				<button href="" onclick="hideNav();" class="hide-btn">Hide<span class="x icon_close"></span></button>
+			</li>
+								
+				<row>
+					<div class="nav-photo sm-hidden">
+						<a href='profile.php?id='><img src='images/logo_vector.png' alt='profile-photo'/></a>					</div>
+				</row>
+			<li>
+				<h3 class="align-right">
+								
+				</h3>
+			</li>
+			<!-- link -->
+			<li>
+				<a href='profile.php?id='>Your Profile</a>			</li>
+			<!-- link -->
+			<li>
+				<a href="mytournaments.php" class="">Your Tournaments</a>
+			</li>
+			<!-- link -->
+			
+			<hr/>
+			<li>
+				<a href='profile.php?id=&page=notifications'>Notifications 
+				 ()				</a>
+			</li>
+			<!-- link -->
+			
+			<li>
+				<a href="support.php" class="">My Tickets</a>
+			</li>
+			<hr/>
+			<!-- link -->
+			<li>
+				<form method="post">
+					<a href="index.php?logout" class="">Sign out</a>
+				</form>
+			</li>
+		</ul>
+</aside>
