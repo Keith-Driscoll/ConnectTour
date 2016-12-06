4c1af6469457c4156eb5a8beef3b730063aab4ff
james_000 <james_000@COLLEGEPC>
2016-12-06 17:53:42 +0000

    changed cols back to 9

--- tournaments.php
+++ tournaments.php
@@ -27,7 +27,7 @@
             ?>
 			
 			<!-- tour list -->
-			<column class="bothColumns" cols="16">
+			<column class="bothColumns" cols="9">
 				<!--Desktop Version Start-->
 				<div class="tournamentContainer xs-hidden">			
 					<div class="tournamentsList">
