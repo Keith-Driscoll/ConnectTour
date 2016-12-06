8d5b753136b8b097cf6093a8dc0ef16230179f47
keith <keith@DESKTOP-2VIEUS0>
2016-12-06 12:59:49 +0000

    test

--- permissions.php
+++ permissions.php
@@ -3,16 +3,21 @@
 //added by keith
 include_once("segments/header.php");
 //
-	// load the login class
-	require_once("classes/Login.php");
-	$login = new Login();
-	if ($login->isUserLoggedIn() == true) {
-	    include("segments/logged_in.php");
-	} else {
-	    include("segments/not_logged_in.php");
-	}
-	$error = "You cannot access this page, as you do not have alpha access. Please apply for access or return to the home page";
-	echo $error;
 ?>
+</head>
+<html>
+    <?php
+	    // load the login class
+	    require_once("classes/Login.php");
+	    $login = new Login();
+	    if ($login->isUserLoggedIn() == true) {
+	        include("segments/logged_in.php");
+	    } else {
+	        include("segments/not_logged_in.php");
+	    }
+	    $error = "You cannot access this page, as you do not have alpha access. Please apply for access or return to the home page";
+	    echo $error;
+    ?>
+</html>
 
 <?php include 'segments/footer.php'; ?>
