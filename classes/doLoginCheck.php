c00fe690b5542178ccca06bdb7dea9ba83556879
keith <keith@DESKTOP-2VIEUS0>
2016-12-06 13:49:16 +0000

    back to normal path

--- classes/doLoginCheck.php
+++ classes/doLoginCheck.php
@@ -4,9 +4,9 @@
 	//checks if player is logged in and displays navbar accordingly
 	$login = new Login();
 	if ($login->isUserLoggedIn() == true) {
-		include '/segments/logged_in.php';
+		include 'segments/logged_in.php';
 	} else {
-	    include '/segments/not_logged_in.php';
+	    include 'segments/not_logged_in.php';
 	    //header("Location: permissions.php");
 	    exit;
 	}
