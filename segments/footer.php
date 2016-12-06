2b813a77bec976781556470919e4dde145b0ead9
keith <keith@DESKTOP-2VIEUS0>
2016-12-06 17:41:14 +0000

    footer changes - Keith

--- segments/footer.php
+++ segments/footer.php
@@ -9,13 +9,13 @@
 					<column cols="1">
 					<!-- icon alignment -->
 						<!-- icon -->	
-						<a href="https://www.facebook.com/GoodGamingLeagues/">
+						<a href="https://www.facebook.com/">
 							<span aria-hidden="true" class="social_facebook medium" ></span> 
 						</a><!-- ./icon end-->
 					</column>
 					<column cols="1">	
 						<!-- icon -->	
-						<a href="http://www.twitter.com/ggleagues">
+						<a href="http://www.twitter.com/">
 							<span aria-hidden="true" class="social_twitter medium" ></span> 
 						</a><!-- ./icon end-->
 					</column>					
@@ -32,7 +32,7 @@
 				<row centered>
 					<column cols="8">
 						<div class="align-centered inner">
-							<p>&copy; 2015-2016 <a href="index.php">Good Gaming Leagues</a>.</p>
+							<p>&copy; 2015-2016 <a href="index.php">ConnectTour</a>.</p>
 						</div>
 					</div>
 				</row>
