0e3140dbff8b16dea353c61825ec874301fac504
james_000 <james_000@COLLEGEPC>
2016-12-06 17:55:02 +0000

    canged table agaun

--- tournaments.php
+++ tournaments.php
@@ -35,11 +35,11 @@
 										<row>
 											<column cols="1"></column>
 											<column cols="4"><div class="width-12 giveMinWidth">Name</div></column>
-											<column cols="2"><div class="width-12 giveMinWidth">Price</div></column>
+											<column cols="1"><div class="width-12 giveMinWidth">Price</div></column>
 											<column cols="1" class="sm-hidden"><div class="width-12 giveMinWidth">Region</div></column>
 											<column cols="1"><div class="width-12 giveMinWidth">Tour Members</div></column>
 											<column cols="2"><div class="width-12 giveMinWidth">Start date</div></column>
-											<column cols="2"><div class="width-12 giveMinWidth">Start Time</div></column>												
+											<column cols="1"><div class="width-12 giveMinWidth">Start Time</div></column>												
 										</row>						
 						</div>			
 						<div class="dataFillUp">		
