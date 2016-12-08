46a98c91168c1ab755af877a72b67903e6fd7393
james_000 <james_000@COLLEGEPC>
2016-12-08 12:03:22 +0000

    changed mytours filter - jim/robbie

--- mytournaments/applyfilter.php
+++ mytournaments/applyfilter.php
@@ -4,7 +4,7 @@
 	session_start();
     $sql = "SELECT      id, tour_type, tour_name, tour_price,
 					tour_region, tour_members, tour_max, tour_start, StartTime
-			FROM tournament_participants
+			FROM tours
 			JOIN tours ON tournament_participants.Tours_id = tours.id
 			WHERE tournament_participants.Player_id = ".$_SESSION['user_id']."
 				AND tournaments.tournament_checkin_phase >= 0";
