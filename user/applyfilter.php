2c7712a33f12af5ad4626a49d7ddda7e0561277c
keith <keith@DESKTOP-2VIEUS0>
2016-12-09 16:45:30 +0000

    fixed support - Keith

---
+++ user/applyfilter.php
@@ -0,0 +1,24 @@
+<?php 
+	include '../../classes/connections.php';
+	session_start();
+	$db_connection = db_connect();
+	$pid = $_SESSION['user_id'];
+	$sql = "SELECT tickets.ticket_id, tickets.priority, tickets.category, tickets.title,
+			tickets.response_needed, tickets.date_submitted, player.player_username,tickets.last_response
+			FROM tickets
+			JOIN player ON tickets.submitter = player.id
+			WHERE submitter = '".$pid."' ";
+
+			
+	//append filters to text if needed
+	if(isset($_POST['Game']) && $_POST['Game'] != "Choose Game"){
+		//$sql .= " AND tournament_game = '".$_POST['Game']."'";
+	}
+	if(isset($_POST['Region']) && $_POST['Region'] != "Choose Region"){
+		//$sql .= " AND tournament_region = '".$_POST['Region']."'";
+	}
+	$result = $db_connection->query($sql);
+	$rows = mysqli_fetch_all($result);
+	$json = json_encode($rows);
+	echo($json);
+?>
---
+++ user/pagination.php
@@ -0,0 +1,115 @@
+<?php 
+	// edited table formatting to make each row a link to it's respective
+	// tournamentinfoNew.php page.
+?>
+<script>
+	var current_page = 1;
+	var records_per_page = 6;
+	
+	function firstPage(){
+		current_page = 1;
+		changePage(current_page);
+	}	
+	function prevPage() {
+		if (current_page > 1) {
+			current_page--;
+			changePage(current_page);
+		}
+	}
+	function nextPage(){
+		if (current_page < numPages()) {
+			current_page++;
+			changePage(current_page);
+    	}
+	}
+	function lastPage(){
+		current_page = numPages();
+		changePage(current_page);
+	}
+
+	function changePage(page){
+		var btn_start = document.getElementById("firstPage");
+		var btn_next = document.getElementById("nextPage");
+		var btn_prev = document.getElementById("prevPage");
+		var btn_end = document.getElementById("lastPage");
+		// Validate page
+		if(jsonData.length>0){
+			if (page < 1) page = 1;
+			if (page > numPages()) page = numPages();
+		
+			var fullString = new Array();
+			$('#totalPages').html(numPages());
+			for (var i = (page-1) * records_per_page; i < (page * records_per_page); i++) {
+				
+				var array = new Array();
+				if(i < jsonData.length){
+					$.each(jsonData[i], function(k , v) {				
+						array.push(v);				
+					});
+					var date = array[5];
+					var read = array[4];
+					var readstring="Read";
+					if(read==1){
+						readstring="Unread";
+					}
+					else readstring="Read";
+					//Modified by Keith 30/6/2016
+					var stringPart = `<div id="support-ticket" class="oneTournament">
+									
+											<a class="selected-ticket" href="ticket.php?id=`+array[0]+`">	
+												<row centered>
+													<column cols="1">`+array[2]+`</column>
+													<column cols="4">`+array[3]+`</column>
+													<column cols="1" class="sm-hidden">`+readstring+`</column>
+													<column cols="3">`+array[5]+`</column>
+													<column cols="3">`+array[7]+`</column>
+													
+												</row>
+											</a>						
+										</div>	`;
+					fullString.push(stringPart);
+				}	
+				//End
+			}
+			$('.dataFillUp').html(fullString);
+			$('#currentPage').html(current_page);
+			
+			if (page == 1) {
+				btn_prev.style.visibility = "hidden";			
+			} else {
+				btn_prev.style.visibility = "visible";
+			}
+			if (page == 1 || page == 2){
+				btn_start.style.visibility = "hidden";
+			} else {
+				btn_start.style.visibility = "visible";
+			}	
+			if (page == numPages()) {
+				btn_next.style.visibility = "hidden";
+			} else {
+				btn_next.style.visibility = "visible";
+			}
+			
+			if (page == numPages() || page == numPages()-1){
+				btn_end.style.visibility = "hidden";
+			} else {
+				btn_end.style.visibility = "visible";
+			}
+		}else{
+			$('.dataFillUp').html("No results found");
+			$('.mobileDataFillUp').html("No results found");
+			btn_prev.style.visibility = "hidden";
+			btn_start.style.visibility = "hidden";
+			btn_next.style.visibility = "hidden";
+			btn_end.style.visibility = "hidden";
+			btn_prevM.style.visibility = "hidden";
+			btn_startM.style.visibility = "hidden";
+			btn_nextM.style.visibility = "hidden";
+			btn_endM.style.visibility = "hidden";
+		}
+	}
+
+	function numPages(){
+		return Math.ceil(jsonData.length / records_per_page);
+	}
+</script>
