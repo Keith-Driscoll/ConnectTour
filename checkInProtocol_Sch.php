f2a7b397b99659d2622fb053468a34894b6f4693
keith <keith@DESKTOP-2VIEUS0>
2016-12-09 14:40:30 +0000

    checkin protocol - Keith

--- checkInProtocol_Sch.php
+++ checkInProtocol_Sch.php
@@ -1,38 +1,38 @@
 <?php
-	$servername = "gglmysql.cloudapp.net";
-	$username = "ggl_adam";
-	$password = "skwreapm0011";
+    $servername = "127.0.0.1:49354";
+	$username = "azure";
+	$password = "6#vWHD_$";
 	$dbname = "ggl_main";
 
 	// Create connection
 	$db_connection = new mysqli($servername, $username, $password, $dbname);
 	echo '1';
-	$sql = "SELECT id 
-			FROM tournaments
-			WHERE unix_timestamp(tournament_start_timestamp) - unix_timestamp(NOW())<=3600 
+	$sql = "SELECT id
+			FROM tours
+			WHERE unix_timestamp(tournament_start_timestamp) - unix_timestamp(NOW())<=3600
 			AND tournament_checkin_phase=0";
 	echo '2';
 	$tournament = $db_connection->query($sql);
-	
+
 	echo '3';
 	if($tournament->num_rows>0){
 		echo '4';
 		while($t_row=$tournament->fetch_assoc()){
-			
-			$sql= "UPDATE tournaments 
+
+			$sql= "UPDATE tournaments
 				   SET tournament_checkin_phase = 1
 				   WHERE id='".$t_row['id']."'";
 			$db_connection->query($sql);
-			
+
 			echo '5';
 			$sql = "INSERT INTO chat_session(chat_class,class_id)
 					VALUES('tournament_lobby','".$t_row['id']."')";
 			echo '6';
 			$db_connection->query($sql);
-			
+
 			$chat_id = $db_connection->insert_id;
 			echo '7';
-			$sql = "SELECT * FROM tournament_participants 
+			$sql = "SELECT * FROM tournament_participants
 					WHERE Tournaments_id='".$t_row['id']."'";
 			$participants = $db_connection->query($sql);
 			echo '8';
@@ -44,9 +44,9 @@
 					$sql = "INSERT INTO chat_participants(chat_id,player_id,priviledges)
 							VALUES ('".$chat_id."','".$p_row['Player_id']."','".$p_row['checked_in']."')";
 					$db_connection->query($sql);
-				}				
-			}			
+				}
+			}
 		}
 	}
-	
+
 ?>
