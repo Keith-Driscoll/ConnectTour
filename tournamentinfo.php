13012266ea2a9c486804c55c78bbf548ca6bfee6
Robbie Jenkinson <Robbie Jenkinson@ROBBIEJENKINSON>
2016-12-09 10:56:24 +0000

    break fix hopefully - rob

--- tournamentinfo.php
+++ tournamentinfo.php
@@ -1,64 +1,64 @@
 <?php 
 
-    /* list of files included in this page
-        bracketgeneration.php
-    */
-	
-	//require_once("classes/doLoginCheck.php");
-	
-    require_once 'segments/header.php';
-    require_once 'classes/connections.php';
-    require_once 'bracketgeneration.php';
-    $db_connection = db_connect();
-	include 'classes/Login.php';
-	$login = new Login();
-	session_start();
-    //holds the id of the tour, pulled from url
-    $t_id = intval($_GET['id']);
-    //holds user_id stored in session if the player is logged in
-    $p_id = intval($_SESSION['user_id']);
-    //query retrieves all data relating to the tournament $t_id
-    $sql = "SELECT * FROM tours WHERE id = '".$t_id."'";
-    $result = $db_connection->query($sql);
-    $row = $result->fetch_assoc();
-	$game = $row['tour_type'];
-	
-	if($game!="Sport"){ //TODO Should be replaced with a list of supported games
-		$game = "general";
-	}
-	
-	//$checkin =$row;
-    //query retrieves the ids of all players registered to the tournament
-    $sql = "SELECT Player_id FROM tournament_participants WHERE Player_id = '".$p_id."' AND Tournaments_id = '".$t_id."'";
-    $res = $db_connection->query($sql);
-	$text = "leave";
-	$joining=0;
-	$inTournament = 1;
-	if($res->num_rows==0){
-		$text="join";
-		$joining=1;
-		$inTournament = 0;
-	} 
-	$loggedIn = $login->isUserLoggedIn();
-    //if user tries to join, is logged in, is not already part of the tournament, and there is space in the tournament;
-    if (isset($_POST['join']) && ($login->isUserLoggedIn() == true) && ($test->num_rows == 0) && ($row['tour_members'] < $row['tour_max'])){
-        //query adds player to tournament participants
-        $sql = "INSERT INTO tournament_participants (Player_id, Tournaments_id) VALUES ('".$p_id."', '".$t_id."')";
-        $enter = $db_connection->query($sql);
+/* list of files included in this page
+bracketgeneration.php
+ */
 
-        //query increments number of players in tournament by 1
-        $sql = "UPDATE tours SET tour_members=tour_members+1";
-        $enter = $db_connection->query($sql);
-    }
-	
-	$bracketHeight = pow(2,ceil(log($row['tour_members'],2)));
+//require_once("classes/doLoginCheck.php");
 
-	$sql = "SELECT is_admin FROM player WHERE id=$p_id";
-	$adminres= $db_connection->query($sql);
-	$adminrow = $adminres->fetch_assoc();
-	$isAdmin = $adminrow['is_admin'];
-	//includes necessary file to handle payment of entry fee if required
-		include 'payments/entry_fee.php';
+require_once 'segments/header.php';
+require_once 'classes/connections.php';
+require_once 'bracketgeneration.php';
+$db_connection = db_connect();
+include 'classes/Login.php';
+$login = new Login();
+session_start();
+//holds the id of the tournament, pulled from url
+$t_id = intval($_GET['id']);
+//holds user_id stored in session if the player is logged in
+$p_id = intval($_SESSION['user_id']);
+//query retrieves all data relating to the tournament $t_id
+$sql = "SELECT * FROM tours WHERE id = '".$t_id."'";
+$result = $db_connection->query($sql);
+$row = $result->fetch_assoc();
+$game = $row['tour_type'];
+
+if($game!="Sport"){ //TODO Should be replaced with a list of supported games
+    $game = "general";
+}
+
+//$checkin =$row;
+//query retrieves the ids of all players registered to the tournament
+$sql = "SELECT Player_id FROM tournament_participants WHERE Player_id = '".$p_id."' AND Tournaments_id = '".$t_id."'";
+$res = $db_connection->query($sql);
+$text = "leave";
+$joining=0;
+$inTournament = 1;
+if($res->num_rows==0){
+    $text="join";
+    $joining=1;
+    $inTournament = 0;
+} 
+$loggedIn = $login->isUserLoggedIn();
+//if user tries to join, is logged in, is not already part of the tournament, and there is space in the tournament;
+if (isset($_POST['join']) && ($login->isUserLoggedIn() == true) && ($test->num_rows == 0) && ($row['tour_members'] < $row['tour_max'])){
+    //query adds player to tournament participants
+    $sql = "INSERT INTO tournament_participants (Player_id, Tournaments_id) VALUES ('".$p_id."', '".$t_id."')";
+    $enter = $db_connection->query($sql);
+
+    //query increments number of players in tournament by 1
+    $sql = "UPDATE tours SET tour_members=tour_members+1";
+    $enter = $db_connection->query($sql);
+}
+
+$bracketHeight = pow(2,ceil(log($row['tour_members'],2)));
+
+$sql = "SELECT is_admin FROM player WHERE id=$p_id";
+$adminres= $db_connection->query($sql);
+$adminrow = $adminres->fetch_assoc();
+$isAdmin = $adminrow['is_admin'];
+//includes necessary file to handle payment of entry fee if required
+include 'payments/entry_fee.php';
 ?>
 
 <script src='js/jquery.min.js'></script>
@@ -103,8 +103,8 @@
 </head>
 <body>
 <?php 
-	require_once "segments/navigation.php"; 
-	require_once "payments/entry_fee.php";
+require_once "segments/navigation.php"; 
+require_once "payments/entry_fee.php";
 ?>
 <script>
 	function joinAndLeave(){
@@ -174,15 +174,15 @@
 					</div>
 				</div><!-- ./players detail end-->
 				<!-- entry fee detail -->
-				 <div class="width-6 piece">
+				 <!--<div class="width-6 piece">
 					<div class="nameOfDetail piece">
 						Tour Cost
 					</div>
 					<div class="actualDetail piece">
-						€/*<?=/* $row['tournament_entry_fee'];*/?>*/
+						€<?= $row['tournament_entry_fee'];?>
 					</div>
 				</div><!-- ./ entry fee detail end--> -->
-				<!-- start date detail -->-->
+				<!-- start date detail -->
 				<div class="width-6 piece">
 					<div class="nameOfDetail piece">
 						Start Date
@@ -190,11 +190,20 @@
 					<div class="actualDetail piece">
 						<!-- timestamp -->
 						<?php $timestamp =$row['tour_start'];
-							/*$justDate = substr($timestamp,0,10);	*/
-							echo $timestamp?>
-					</div><!-- ./timestamp detail end --> 
+                              /*$justDate = substr($timestamp,0,10);	*/
+                              echo $timestamp?>
+					</div><!-- ./timestamp detail end --> hh
 				</div>
-               
+                <div class="width-6 piece">
+                    <div class="nameOfDetail piece">
+                       <!--StartTime-->
+                    </div>
+                    <div class="actualDetail piece">
+                        <?php /*$timestamp =$row['StartTime'];
+                        $justTime = substr($timestamp,0,10);	
+                        echo $justTime */?>
+                    </div>
+                </div>
             
             
             
@@ -212,18 +221,18 @@
 				
 				<div  class="width-6 piece ">
 					<?php
-						if($loggedIn){
-							if($row['tournament_checkin_phase']!=2){ ?><div onclick="joinAndLeave()" class="nameOfDetail piece <?=$text?>" >
+                    if($loggedIn){
+                        if($row['tournament_checkin_phase']!=2){ ?><div onclick="joinAndLeave()" class="nameOfDetail piece <?=$text?>" >
 						<!--<input type='button' class='sendBtn btn'>-->
 						<?php
 							echo $text;
-						?>
+                        ?>
 					</div>
 					<?php }else{
-						?>
+                    ?>
 						<div class="nameOfDetail piece ongoing">This tour is ongoing</div>
 					<?php }
-						}?>
+                    }?>
 				</div>
 				
 				
@@ -238,12 +247,12 @@
 			</div>
 			<div class="infoBody chatbox">
 				<?php 
-					$sql = "SELECT chat_id FROM chat_session WHERE class_id = ".$t_id." AND chat_class = 'tournament_lobby'";
-					$result = $db_connection->query($sql);
-					$res = $result->fetch_assoc();
-					$chatID = $res['chat_id']; 
-					include 'chatbox.php';
-				?>
+                $sql = "SELECT chat_id FROM chat_session WHERE class_id = ".$t_id." AND chat_class = 'tournament_lobby'";
+                $result = $db_connection->query($sql);
+                $res = $result->fetch_assoc();
+                $chatID = $res['chat_id']; 
+                include 'chatbox.php';
+                ?>
 			</div>
 		</column><!-- ./right column end -->
 		
@@ -279,27 +288,27 @@
 							</form>
 						<?php } ?>
 						<?php 
-							$savedRow = $row;
-							if($savedRow['tournament_checkin_phase']==2){
-						?>
+                        $savedRow = $row;
+                        if($savedRow['tournament_checkin_phase']==2){
+                        ?>
 						<?php 
 							include 'tournamentInfo/matchinfo.php';
-							}else{
-								echo "Tour hasn't begun yet";
-							}
-						?>
+                        }else{
+                            echo "Tour hasn't begun yet";
+                        }
+                        ?>
 					</div>
 					<div id="theBrackets">
 						<?php 
-							if($savedRow['tournament_checkin_phase']==2){
-						?>
+                        if($savedRow['tournament_checkin_phase']==2){
+                        ?>
 							
 							<iframe width="" class="iframe bracketFrame" <?php echo'src="bracket_test.php?id='.$t_id.'"'?>></iframe>
 						<?php
-							}else{
-									echo "Tour has not begun yet.";
-							}
-						?>	
+                        }else{
+                            echo "Tour has not begun yet.";
+                        }
+                        ?>	
 					</div>
 					<div id="theParticipants">
 						<?php include 'tournamentInfo/participants.php';?>
@@ -313,6 +322,6 @@
 			</column><!-- ./column end -->
 		</row><!-- ./row end -->
 <?php 
-	//include "games/hearthstone/match_reporting.php";
-	include 'segments/footer.php';
+//include "games/hearthstone/match_reporting.php";
+include 'segments/footer.php';
 ?>
